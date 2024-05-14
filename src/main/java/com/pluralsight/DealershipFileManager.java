b95c89bb0250c04496b22048ccfed8901a555a22
AbiramiThavamani <athavamani@pgh.yearup.org>
2024-05-13 21:47:45 -0400

    added a user inter face methods

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,67 @@
 package com.pluralsight;
 
+
+
+import java.io.*;
+
+
 public class DealershipFileManager {
-}
+    private String fileName;
+
+    public DealershipFileManager() {
+        this.fileName = fileName;
+
+    }
+
+    // method should read dealership data from file and create a dealership object
+     public  Dealership getDealership() {
+
+         Dealership dealership = null;
+         try {
+
+             BufferedReader bufferedReader = new BufferedReader(new FileReader("dealership.csv"));
+
+             String line;
+             if ((line = bufferedReader.readLine()) != null) {
+                 String[] part = line.split("\\|");
+                 String name = part[0];
+                 String address = part[1];
+                 String phone = part[2];
+                 dealership = new Dealership(name, address, phone);
+
+                 // read vehicle data and add vehicle to the dealership
+
+                 while ((line = bufferedReader.readLine()) != null) {
+                     String[] parts = line.split("\\|");
+                     if (parts.length == 8) {
+                         int vin = Integer.parseInt(parts[0]);
+                         int year = Integer.parseInt(parts[1]);
+                         String make = parts[2];
+                         String model = parts[3];
+                         String vehicleType = parts[4];
+                         String color = parts[5];
+                         int odometer = Integer.parseInt(parts[6]);
+                         double price = Double.parseDouble(parts[7]);
+                         dealership.addVehicle(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
+
+                     }
+
+                 }
+             }
+
+             bufferedReader.close();
+
+         } catch (Exception e) {
+             e.printStackTrace();
+
+         }
+         return dealership;
+     }
+
+    // saveDealerShip method
+
+    public void saveDealership(Dealership dealership) {
+    }
+
+    }
+
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,149 @@
 package com.pluralsight;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
 
-}
+        private Dealership dealership  ;
+        private Scanner scanner;
+
+    public UserInterface(Dealership dealership) {
+        this.dealership = dealership;
+        this.scanner = new Scanner(System.in);
+        init();
+
+    }
+
+    private void init(){
+        DealershipFileManager dealershipFileManager = new DealershipFileManager();
+        dealership = dealershipFileManager.getDealership();
+
+    }
+
+    public void displayMenu(){
+        System.out.println("Welcome to the Car Dealership!");
+        System.out.println("1. Get by price request");
+        System.out.println("2. Get by make model request");
+        System.out.println("3. Get by year request");
+        System.out.println("4. Get by color request");
+        System.out.println("5. Get by mileage request");
+        System.out.println("6. Get by vehicle typeRequest");
+        System.out.println("7. Get all vehicles request");
+        System.out.println("8.add vehicle request");
+        System.out.println("9. remove vehicle request");
+        System.out.println("10. helper vehicle ");
+        System.out.println("11. exit");
+        System.out.println("enter your choice: ");
+
+    }
+
+ //   public void userInputRequest(){
+        boolean exit = false;
+        while (!exit){
+            displayMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice){
+                case 1:
+                    processGetByPriceRequest();
+                    break;
+                case 2:
+                    processGetByMakeModelRequest();
+                    break;
+
+                case 3:
+                    processGetByYearRequest();
+                    break;
+
+                case 4:
+                    processGetByColorRequest();
+                    break;
+
+                case 5:
+                    processGetByMileageRequest();
+                    break;
+
+                case 6:
+                    processGetByVehicleTypeRequest();
+                    break;
+
+                case 7:
+                    processGetByAllVehiclesRequest();
+                    break;
+
+                case 8:
+                    processAddVehicleRequest();
+                    break;
+
+                case 9:
+                    processRemoveVehicleRequest();
+                    break;
+
+                case 10:
+                    displayVehicleHelper();
+                    break;
+
+                case 11:
+                    exit = true;
+                    System.out.println("Thank you for visiting!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. please try again.");
+                    break;
+            }
+        }
+    }
+
+
+    private void processGetByPriceRequest(){
+
+    }
+
+    private void processGetByMakeModelRequest(){
+
+    }
+
+    private void processGetByYearRequest(){
+
+    }
+
+    private void processGetByColorRequest(){
+
+    }
+
+    private  void processGetByMileageRequest(){
+
+    }
+
+    private void processGetByVehicleTypeRequest(){
+
+    }
+
+    private void processGetByAllVehiclesRequest(){
+
+    }
+
+    private void processAddVehicleRequest(){
+
+    }
+    private void processRemoveVehicleRequest(){
+
+    }
+
+    //private void displayVehicleHelper(List<Vehicle> vehicles){
+        System.out.println("Listing vehicles:");
+        for (Vehicle vehicle : vehicles){
+            System.out.println(vehicle);
+        }
+
+        }
+
+       // private void processGetAllVehicleRequest(){
+        List<Vehicle> allVehicles = dealership.getAllVehicles();
+        displayVehicleHelper(allVehicles);
+        }
+
+    }
+
